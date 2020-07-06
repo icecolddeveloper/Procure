a70570715e3e4c03f0220f287873eb6078d837f8
Michael Nwigwe <icecolddeveloper@gmail.com>
2020-07-07 00:52:09 +0100

    Adde craiglist search funtionality

--- my_app/views.py
+++ my_app/views.py
@@ -20,15 +20,19 @@ def new_search(request):
     data = response.text
     soup = BeautifulSoup(data, features='html.parser')
 
-    post_listings = soup.find_all('li', {'class': 'result-row'})
+    post_listings = soup.find_all('a', {'class': 'result-title'})
+
+    post_title = post_listings[0].find(class_='result-title').text
+    post_url = post_listings[0].find('a').get('href')
+    post_price = post_listings[0].find(class_='result-price').text
 
     final_postings = []
 
     for post in post_listings:
-        post_title = post.find(class_='result-title').text
+        post_title = post.find('a', {'class': 'result-title'}).text
         post_url = post.find('a').get('href')
 
-        if post.find(class_='result-price'):
+        if post.find(class_='result-title'):
             post_price = post.find(class_='result-price').text
         else:
             post_price = 'N/A'
@@ -40,11 +44,11 @@ def new_search(request):
         else:
             post_image_url = 'https://craigslist.org/images/peace.jpg'
 
-        final_postings.append((post_title, post_url, post_price, post_image_url))
+    final_postings.append((post_title, post_url, post_price))
 
     stuff_for_frontend = {
         'search': search,
-        'final_postings': final_postings,
+        'final_posting': final_postings,
     }
 
     return render(request, 'my_app/new_search.html', stuff_for_frontend)
